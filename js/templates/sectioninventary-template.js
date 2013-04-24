5024614bfbaf4f6fa70bcbfbb1434abade994601
Pablo Molina <p2kmgcl@gmail.com>
2013-04-24 17:49:15 +0200

    - Añadidos todos los elementos al inventario - Organizada y separada la hoja de estilos

--- js/templates/sectioninventary-template.js
+++ js/templates/sectioninventary-template.js
@@ -1,5 +1,9 @@
 window.alibelTPV.templates.sections.Inventary = '\
-    <input type="search" id="inventarySearch" placeholder="Escribe el nombre de un ítem o su código..." />\n\
-\n\
+    <form id="inventaryMenu">\n\
+        <input type="search" id="inventarySearch" placeholder="Escribe el nombre de un ítem o su código..." />\n\
+        <input type="radio" name="inventaryFilter" id="inventaryFilterAll" /><label for="inventaryFilterAll">Todos</label>\n\
+        <input type="radio" name="inventaryFilter" id="inventaryFilterLowStock" /><label for="inventaryFilterLowStock">Stock bajo</label>\n\
+        <input type="radio" name="inventaryFilter" id="inventaryFilterNoStock" /><label for="inventaryFilterNoStock">Sin stock</label>\n\
+    </form>\n\
     <ul id="inventaryItemList"></ul>\n\
 ';
