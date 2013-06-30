90116f260f8d9e578c4e63b271296f40e84e2194
Pablo Molina <p2kmgcl@gmail.com>
2013-06-30 18:09:01 +0200

    Movido el contenido de los cuadros de diálogo a un nuevo directorio.

--- js/templates/dialogs/newsell_edititem.js
+++ js/templates/dialogs/newsell_edititem.js
@@ -1,15 +1,15 @@
 alibel.templates.ItemCartDialog = '\
-    <label for="itemCartDialogName">Nombre</label>\
+    <label for="itemCartDialogName"><%= __("name") %></label>\
     <span class="name"><%= name %></span>\
     \
-    <label for="itemCartDialogQuantity" class="quantity">Cantidad</label>\
+    <label for="itemCartDialogQuantity" class="quantity"><%= __("quantity") %></label>\
     <input type="number" min="0" max="<%= maxQuantity %>" step="1" value="<%= quantity %>" id="itemCartDialogQuantity" />\
     <span class="quantityUnit"><%= units.toLowerCase() %></span>\
     \
-    <label for="itemCartDialogPrice" class="price">Precio</label>\
+    <label for="itemCartDialogPrice" class="price"><%= __("price") %></label>\
     <input type="number" min="0" value="<%= price.toFixed(2) %>" step="0.01" id="itemCartDialogPrice" />\
     <span class="priceUnit">€ / <%= unit.toLowerCase() %></span>\
     \
-    <label for="id="itemCartDialogFinalPrice">Total</label>\
+    <label for="id="itemCartDialogFinalPrice"><%= __("totalPrice") %></label>\
     <span class="finalPrice" id="itemCartDialogFinalPrice"><%= (quantity * price).toFixed(2) %></span>\
 ';
